e5197384f584e1f04bc1e7d8c2d644187650b63b
Genisys <guh0613@icloud.com>
2025-07-05 20:09:04 +0800

    1.1

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'http://localhost:8000';
-export const APP_VERSION = '1.01';
-export const VERSION_CODE = '1.01';
+export const APP_VERSION = '1.1';
+export const VERSION_CODE = '1100';
